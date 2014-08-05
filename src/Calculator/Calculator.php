56ccf213230457874215251a8fe705c8b39c79e6
David Fritsch <david@makethewebwork.com>
2014-08-04 17:27:51 -0700

    That seems to do the trick as long as white space is provided...

--- src/Calculator/Calculator.php
+++ src/Calculator/Calculator.php
@@ -6,13 +6,13 @@ class Calculator {
     protected static $response = null;
     private static $operators = array(
         0 => array(
-            '*',
-            '/',
-            '%'
+            '*' => 'multiply',
+            '/' => 'divide',
+            '%' => 'modulus'
         ),
         1 => array(
-            '+',
-            '-'
+            '+' => 'add',
+            '-' => 'subtract'
         )
     );
 
@@ -89,13 +89,107 @@ class Calculator {
     * Returns:
     * Calculated value
     * or throws an exception if parsing error
+    *
+    * THIS IS A VERY EARLY IMPLEMENTATION!
+    * Features still to be solved:
+    * -
+    * -
+    * -
+    * -
+    * -
+    * -
+    * -
+    * -
     */
     protected function process()
     {
-        $value = 0;
+        $working_input = (string)self::$input;
 
-        // TODO processing algorithm
+        if (!$working_input) {
+            throw new \RuntimeException('No string to process', 500);
+        }
 
-        return self::$response = $value;
+        // regularize whitespace
+        $working_input = trim(preg_replace('/\s+/', ' ', $working_input));
+
+        // split on whitespace (should be number, operator, number, operator... pattern)
+        $elements = explode(' ', $working_input);
+
+        // loop once on operators, for high level actions (*, /, %)
+        $elements = self::processOperators($elements, self::$operators[0]);
+
+        // loop second time for lower level actions (+, -)
+        $elements = self::processOperators($elements, self::$operators[1]);
+
+        // should end with an array with one element
+        if (sizeof($elements) === 1) {
+            return self::$response = $elements[0];
+        } else {
+            throw new \RuntimeException('Error processing input string. Invalid value found.', 500);
+        }
+    }
+
+    private static function processOperators(array $elements, array $operators)
+    {
+        // loop over operators and process as fitting
+        $length = sizeof($elements);
+        for ($i = 0; $i < $length; $i++) {
+            // odd keys contain operators
+            if (!($i%2)) {
+                continue;
+            }
+
+            // not processing lower level functions yet
+            if (!array_key_exists($elements[$i], $operators)) {
+                continue;
+            }
+
+            // convert operator into action
+            $operator = $operators[$elements[$i]];
+
+            // perform operation
+            $elements[$i-1] = call_user_func(array(self, $operator), $elements[$i-1], $elements[$i+1]);
+
+            // clean up processed operator and secondary value
+            unset($elements[$i]);
+            unset($elements[$i+1]);
+
+            // squish back down array as needed
+            $elements = array_values($elements);
+            $length = sizeof($elements);
+            $i--;
+        }
+
+        return $elements;
+    }
+
+    // Method to add two numbers
+    private static function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    // Method to subtract two numbers
+    private static function subtract($a, $b)
+    {
+        return self::add($a, (-$b));
+    }
+
+    // Method to add two numbers
+    private static function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    // Method to subtract two numbers
+    private static function divide($a, $b)
+    {
+        return self::multiply($a, (1/$b));
+    }
+
+    // Method to return the remained on two numbers
+    private static function modulus($a, $b)
+    {
+        return $a % $b;
     }
 }
--- test.php
+++ test.php
@@ -3,8 +3,14 @@ require_once('vendor/autoload.php');
 
 use Calculator\Calculator;
 
-$test_string = '1 + 1 - 4 * 4';
-
+$test_string = '1 +  1 - 4 * 4';
 $answer = Calculator::calculate($test_string);
+echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
 
-echo '<p>The answer for "'. $test_string .'" is ' . $answer . '</p>';
+$test_string = " 4 * \t 5 + \n6 / 7";
+$answer = Calculator::calculate($test_string);
+echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
+
+$test_string = '   2   +   7 /   6 + 1   * 4';
+$answer = Calculator::calculate($test_string);
+echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
