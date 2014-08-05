98c32d8b5e29de698c2c361499f9225433704c47
David Fritsch <david@makethewebwork.com>
2014-08-04 17:45:23 -0700

    Update test script to test precision

--- test.php
+++ test.php
@@ -8,13 +8,13 @@ $answer = Calculator::calculate($test_string);
 echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
 
 $test_string = " 4 * \t 5 + \n6 / 7";
-$answer = Calculator::calculate($test_string);
+$answer = Calculator::calculate($test_string, 8);
 echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
 
 $test_string = '   2   +   7 /   6 + 1   * 4';
-$answer = Calculator::calculate($test_string);
+$answer = Calculator::calculate($test_string, 1);
 echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
 
 $test_string = '3*6-7+9';
-$answer = Calculator::calculate($test_string);
+$answer = Calculator::calculate($test_string, -1);
 echo '<p>The answer for "'. $test_string .'" is <strong>' . $answer . '</strong></p>';
